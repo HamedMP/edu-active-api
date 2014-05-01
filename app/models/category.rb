2de56ea1df9f6e96726c960d9028280f6b08f1a9
Ali Ismayilov <ali@ismailov.info>
2014-05-01 00:16:21 -0300

    Category children and parent

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ActiveRecord::Base
+  has_many :children, class_name: 'Category',
+                      foreign_key: 'category_id'
+
+  belongs_to :parent, class_name: 'Category',
+                      foreign_key: 'category_id'
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    let!(:parent) { create :category }
+    let!(:child1) { create :category }
+    let!(:child2) { create :category }
+
+    before do
+      parent.children << child1
+      parent.children << child2
+    end
+
+    it 'has children' do
+      expect(parent.children).to have(2).items
+    end
+
+    it 'has parent' do
+      expect(child1.parent).to eql parent
+    end
+  end
 end
