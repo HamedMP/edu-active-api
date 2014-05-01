cd62e4b278e42d066c128c4870b68a8e494e327d
Ali Ismayilov <ali@ismailov.info>
2014-05-01 13:37:01 -0300

    ActiveModelSerializer caching

--- app/serializers/v1/category_serializer.rb
+++ app/serializers/v1/category_serializer.rb
@@ -1,5 +1,11 @@
 class V1::CategorySerializer < ActiveModel::Serializer
+  cached
+
   attributes :title, :slug, :order
 
   has_many :children, serializer: V1::CategorySerializer
+
+  def cache_key
+    [object, scope]
+  end
 end
